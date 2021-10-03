d26e5957e4522a7bb6b3baaf8508639a9af2c1e9
King Chun O <kingchun1991@hotmail.com>
2021-10-03 16:14:22 +0800

    rule

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,35 +1,31 @@
-import { GetStaticProps } from "next"
-import Card from "components/layout/Card"
+import { GetStaticProps } from "next";
+import Card from "components/layout/Card";
 
+const csv2json = require("csvtojson");
 
 function Blog(props: any) {
-    return (
-      <ul>
-        {props.records.map((record: any) => {     
-        //    console.log("Entered");                 
-           // Return the element. Also pass key     
-           return (<Card title={record.title_en} imgSrc={record.imgSrc}/>) 
-        })}
-      </ul>
-    )
-  }
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    // const { slug } = context.params as IParams // no longer causes error
-    const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTMf21Q2u8IXza55EClr4tlEn-hpHwoyZGxqS1Wy9xfjDRF5fy0MGjice0i2ONIaoIdp72pHQem7O6Z/pub?gid=0';
-    // const props = fetch(url + '&single=true&output=csv&headers=0')
-    const csv = '&single=true&output=csv&headers=0';
-    const skipFirstRow = '&range=A2:ZZ';
-    const result = await fetch(url + csv + skipFirstRow);
-    const data = await result.text();
-    const csv2json = require("csvtojson");
-    const records = await csv2json().fromString(data);
-    // console.log(data)
-    // console.log(props)
-    // console.log(records)
-    return { props: { records }}
+  const records = props;
+  return (
+    <ul>
+      {records.map((record: any) => {
+        //    console.log("Entered");
+        // Return the element. Also pass key
+        return <Card title={record.title_en} imgSrc={record.imgSrc} />;
+      })}
+    </ul>
+  );
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const url =
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTMf21Q2u8IXza55EClr4tlEn-hpHwoyZGxqS1Wy9xfjDRF5fy0MGjice0i2ONIaoIdp72pHQem7O6Z/pub?gid=0";
 
+  const csv = "&single=true&output=csv&headers=0";
+  const skipFirstRow = "&range=A2:ZZ";
+  const result = await fetch(url + csv + skipFirstRow);
+  const data = await result.text();
+  const records = await csv2json().fromString(data);
+  return { props: { records } };
+};
 
-export default Blog
+export default Blog;
