d4759c8c0a23099510d2083130e2885cc3a03939
King Chun O <kingchun1991@hotmail.com>
2021-10-03 15:20:47 +0800

    init

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
+  title: "nextarter-chakra-google-sheet-starter",
   titleTemplate: "%s | nextarter-chakra",
   defaultTitle: "nextarter-chakra",
   description: "Next.js + chakra-ui + TypeScript template",
   canonical: "https://nextarter-chakra.sznm.dev",
   openGraph: {
     url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
+    title: "nextarter-chakra-google-sheet-starter",
     description: "Next.js + chakra-ui + TypeScript template",
     images: [
       {
@@ -15,7 +15,7 @@ const defaultSEOConfig = {
         alt: "nextarter-chakra.sznm.dev og-image",
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: "nextarter-chakra-google-sheet-starter",
   },
   twitter: {
     handle: "@sozonome",
---
+++ src/pages/blog.tsx
@@ -0,0 +1,34 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+const csv2json = require('csvtojson');
+import Card from 'components/layout/Card'
+
+function Blog(props) {
+    return (
+      <ul>
+        {props.records.map((record, i) => {     
+        //    console.log("Entered");                 
+           // Return the element. Also pass key     
+           return (<Card title={record.title_en} imgSrc={record.imgSrc}/>) 
+        })}
+      </ul>
+    )
+  }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    // const { slug } = context.params as IParams // no longer causes error
+    const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTMf21Q2u8IXza55EClr4tlEn-hpHwoyZGxqS1Wy9xfjDRF5fy0MGjice0i2ONIaoIdp72pHQem7O6Z/pub?gid=0';
+    // const props = fetch(url + '&single=true&output=csv&headers=0')
+    const csv = '&single=true&output=csv&headers=0';
+    const skipFirstRow = '&range=A2:ZZ';
+    const result = await fetch(url + csv + skipFirstRow)
+    const data = await result.text()
+    const records = await csv2json().fromString(data)
+    // console.log(data)
+    // console.log(props)
+    // console.log(records)
+    return { props: { records }}
+}
+
+
+
+export default Blog
